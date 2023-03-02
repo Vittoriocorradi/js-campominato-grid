9dd8a1727f73ec0af74f34025e2a354ba100b540
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-02 16:02:26 +0100

    Griglia JS

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ FUNZIONI
 */
 
 // Funzione per creare una cella
-function myCreateElement(cell, className, innerText) {
-    const cell = document.createElement(cell);
+function myCreateElement(gameCell, className, innerText) {
+    const cell = document.createElement(gameCell);
     cell.classList.add(className);
     cell.innerText = innerText;
     return cell;
@@ -30,3 +30,17 @@ function elementClick(cell, counter, className) {
 MAIN
 ----------
 */
+
+const startGame = document.getElementById('generate-game');
+const gameContainer = document.querySelector('.container');
+
+startGame.addEventListener('click',
+function() {
+    gameContainer.innerHTML = '';
+
+    for (let i = 1; i <= 100; i++) {
+        const cellCreated = myCreateElement('div', 'cell', i);
+        gameContainer.append(cellCreated);
+    }
+}
+)
