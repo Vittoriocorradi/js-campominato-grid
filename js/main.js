0682b54a1a48141f822a6e682ddeccddbcd07ce9
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-02 15:56:10 +0100

    Funzioni JS

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@ FUNZIONI
 ----------
 */
 
+// Funzione per creare una cella
+function myCreateElement(cell, className, innerText) {
+    const cell = document.createElement(cell);
+    cell.classList.add(className);
+    cell.innerText = innerText;
+    return cell;
+}
+
+// Funzione click sulla cella
+function elementClick(cell, counter, className) {
+    
+    cell.addEventListener('click',
+    function() {
+        console.log(counter);
+        cell.classList.toggle(className);
+    }
+    )
+}
+
 /*
 ----------
 MAIN
